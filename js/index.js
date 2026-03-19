e3f89779b767bb5c1914b0998221aa9cce089550
Valentino Cattaneo Luna <valencattaneo04@gmail.com>
2026-03-19 02:20:01 -0300

    feat: display current year in the footer on page load

--- js/index.js
+++ js/index.js
@@ -2,7 +2,10 @@ import {toggleMenu} from './components/toggleMenu.js'
 import { loadSkills } from './components/loadSkills.js';
 import {loadProyectos} from './components/loadProyectos.js';
 import { toggleLang } from './components/toggleLang.js';
+
+const currentYear = new Date().getFullYear();
 document.addEventListener("DOMContentLoaded", async() => {
+    document.getElementById("copy-year").textContent = currentYear;
     toggleMenu();
     loadSkills();
     const idioma = document.getElementById("btn_idioma").dataset.lang
