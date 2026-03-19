8d3711f9906113961309ccbf47de7f8353809211
Valentino Cattaneo Luna <valencattaneo04@gmail.com>
2026-03-19 02:20:11 -0300

    feat: improve language toggle functionality and clean up code formatting

--- js/components/toggleLang.js
+++ js/components/toggleLang.js
@@ -7,32 +7,32 @@ export async function toggleLang() {
     const textToChange = document.querySelectorAll("[data-section]")
     const btn_cv_desktop = document.querySelector(".btn_cv_desktop")
     const btn_cv_mobile = document.querySelector(".btn_cv_mobile")
+    const page_lang = document.documentElement;
 
 
 
-
-    
 
     btn_idioma.addEventListener("click", async () => {
-        
+
         if (btn_idioma.dataset.lang === "es") {
+            page_lang.lang = "en"; 
             text_btn_idioma.textContent = "EN"
             btn_idioma.dataset.lang = "en"
-            
-            btn_cv_desktop.href="./assets/docs/CV_EN_ValentinoCattaneoLuna.pdf"
-            btn_cv_mobile.href="./assets/docs/CV_EN_ValentinoCattaneoLuna.pdf"
-            btn_cv_desktop.download="CV_EN_ValentinoCattaneoLuna.pdf"
-            btn_cv_mobile.download="CV_EN_ValentinoCattaneoLuna.pdf"
-            
+
+            btn_cv_desktop.href = "./assets/docs/CV_EN_ValentinoCattaneoLuna.pdf"
+            btn_cv_mobile.href = "./assets/docs/CV_EN_ValentinoCattaneoLuna.pdf"
+            btn_cv_desktop.download = "CV_EN_ValentinoCattaneoLuna.pdf"
+            btn_cv_mobile.download = "CV_EN_ValentinoCattaneoLuna.pdf"
+
         }
         else {
+            page_lang.lang = "es";
             text_btn_idioma.textContent = "ES"
             btn_idioma.dataset.lang = "es"
-            
-            btn_cv_desktop.href="./assets/docs/CV_ES_ValentinoCattaneoLuna.pdf"
-            btn_cv_mobile.href="./assets/docs/CV_ES_ValentinoCattaneoLuna.pdf"
-            btn_cv_desktop.download="CV_ES_ValentinoCattaneoLuna.pdf"
-            btn_cv_mobile.download="CV_ES_ValentinoCattaneoLuna.pdf"
+            btn_cv_desktop.href = "./assets/docs/CV_ES_ValentinoCattaneoLuna.pdf"
+            btn_cv_mobile.href = "./assets/docs/CV_ES_ValentinoCattaneoLuna.pdf"
+            btn_cv_desktop.download = "CV_ES_ValentinoCattaneoLuna.pdf"
+            btn_cv_mobile.download = "CV_ES_ValentinoCattaneoLuna.pdf"
 
         }
 
